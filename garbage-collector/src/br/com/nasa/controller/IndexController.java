658c79f337a70ffde53416cb6aa0ba81236e7648
leandro <leandrosiqueira.rs@gmail.com>
2018-01-24 13:16:32 -0300

    adicionado tela index

---
+++ garbage-collector/src/br/com/nasa/controller/IndexController.java
@@ -0,0 +1,12 @@
+package br.com.nasa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class IndexController {
+	@RequestMapping("/Index")
+	public String exibirIndex() {
+		System.out.println("tela de inicio.");
+	return "/index";
+	}
+}
