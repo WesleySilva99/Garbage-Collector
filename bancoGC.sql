ce2a1810693cd5d53f9373183e872770d9965bad
gleibsonfreitas <33529364+gleibsonfreitas@users.noreply.github.com>
2018-02-14 22:45:57 -0200

    Update bancoGC.sql
    BANCO DE DADOS ATUALIZADO NÃO MUDEM ELE....

--- bancoGC.sql
+++ bancoGC.sql
@@ -5,18 +5,18 @@ use gc;
 create table endereco(
 
 id int auto_increment,
-bairro varchar(50) not null,
+bairro varchar(20) not null,
 complemento varchar(10) not null,
-rua varchar(20) not null,
-numero varchar(20) not null,
-cep varchar(10) not null,
+rua varchar(50) not null,
+numero varchar(6) not null,
+cep varchar(9) not null,
 primary key (id)
 );
 
 create table cliente(
 
 id int auto_increment,
-nome varchar(45) not null,
+nome varchar(40) not null,
 cpf varchar(14) not null,
 dt_nasc date not null,
 login varchar(10) not null,
@@ -34,14 +34,15 @@ id int auto_increment,
 nome varchar(45) not null,
 cpf varchar(14) not null,
 rg varchar(10) not null,
-telefone varchar(14) not null,
-sexo char not null,
+telefone varchar(15) not null,
+sexo varchar(15) not null,
 n_abilitacao varchar(11) not null,
 cat_abilitacao varchar(4) not null,
+dataVencimento date not null,
 CHASI varchar(30) not null,
 placa varchar(7) not null,
 marca varchar(20) not null,
-ano_veiculo date not null,
+ano_veiculo varchar(4) not null,
 email varchar(45) not null,
 login varchar(10) not null,
 senha varchar(32) not null,
@@ -53,7 +54,7 @@ foreign key(id_endereco) references endereco(id)
 create table pedido(
 
 id int auto_increment,
-tipoColeta varchar(8) not null,
+tipoColeta varchar(50) not null,
 descricao varchar(100) not null,
 quantidade int not null,
 endereco varchar(50) not null,
@@ -64,3 +65,8 @@ primary key (id)
 
 );
 
+select * from pedido;
+select * from cliente;
+drop database gc;
+
+select * from endereco;
